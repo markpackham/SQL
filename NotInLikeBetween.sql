68cb9b2add5b3c524d3585d3d12f646887cc731c
Mark Packham <markpackham1@gmail.com>
2018-03-01 17:51:33 +0000

    revise BETWEEN

--- NotInLikeBetween.sql
+++ NotInLikeBetween.sql
@@ -13,3 +13,11 @@ WHERE city LIKE ('Ne_ York');
 
 SELECT * FROM customers
 WHERE city LIKE ('% York');
+
+--Filter BETWEEN 2 values to show everyone ranged from age wise from 50-300
+SELECT * FROM customers
+WHERE age BETWEEN 50 AND 300;
+
+--BETWEEN also works with timestamps
+SELECT * FROM orders
+WHERE orderDate BETWEEN '2018-02-28 17:45:27' AND '2018-03-28';
