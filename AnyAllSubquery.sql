b2aca8e4e3547c90770c5a17c9a523d3c2c98b6e
Mark Packham <markpackham1@gmail.com>
2018-03-01 12:18:30 +0000

    And, Not, Or

--- AnyAllSubquery.sql
+++ AnyAllSubquery.sql
@@ -1,3 +1,9 @@
+--The sub query gives the WHERE statement what it is looking for
+--Here we are looking for the youngest customer
+SELECT firstName, lastName
+FROM customers
+WHERE age = (SELECT MIN(age) FROM customers);
+
 --ANY and ALL work with WHERE and HAVING clauses
 --ANY will be true if any of the subqueries are true
 
---
+++ NullAndOr.sql
@@ -0,0 +1,18 @@
+--AND will only give results if both criteria are true, this will fail
+SELECT firstName FROM customers
+WHERE state='New York' AND state='Planet Earth';
+
+--NOT turns the statement into a negative so will have the opposite effect to the above
+SELECT firstName FROM customers
+WHERE state='New York' AND NOT state='Planet Earth';
+
+--OR will allow any results that is true to show providing 1 statement is true
+--other statements can also be true or false and it will still give back results
+SELECT firstName FROM customers
+WHERE state='New York' OR state='Planet Earth';
+
+--Find all people who live in the state of New York, must be in any of the following cities listed
+--They must not come from Atlantis
+SELECT firstName FROM customers
+WHERE state='New York' AND (city='New York' OR city='Moscow' OR city='Albion')
+AND NOT state = 'Atlantis';
